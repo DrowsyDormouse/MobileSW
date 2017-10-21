fd6f6f297c9d1ad800928f78cc4187ca243f9ea4
DrowsyDormouse <tpzkdizl@gmail.com>
2017-10-21 23:27:05 +0900

    time line working...

--- app/src/main/java/com/example/song/mobilesw/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/song/mobilesw/MyItemRecyclerViewAdapter.java
@@ -70,8 +70,9 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
+
+            mIdView = (TextView) view.findViewById(R.id.title);
+            mContentView = (TextView) view.findViewById(R.id.body);
         }
 
         @Override
